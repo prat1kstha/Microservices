9324bde2818e0e669da0c99b70a308af1176c433
Pratik Shrestha <animezon.stha@gmail.com>
2022-03-05 19:24:33 +0545

    Added Validations

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -21,13 +21,19 @@ namespace Play.Catalog.Service.Controllers
         public IEnumerable<ItemDto> Get()
         {
             return items;
-
         }
 
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
-            return items.Where(item => item.Id == id).SingleOrDefault();
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
         }
 
         [HttpPost]
@@ -43,6 +49,11 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
         {
             var existingItem = items.Where(i => i.Id == id).SingleOrDefault();
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
 
             var updatedItem = existingItem with
             {
@@ -61,6 +72,12 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Delete(Guid id)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            
             items.RemoveAt(index);
 
             return NoContent();
